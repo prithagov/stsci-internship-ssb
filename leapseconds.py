7365cf8a865b9b45e06d1660a2bc57c20f930dd2
prithagov <prithagov@yahoo.com>
2012-06-28 08:58:49 -0700

    Now outputs first line of text as a tuple

--- leapseconds.py
+++ leapseconds.py
@@ -1,6 +1,17 @@
+import re 
+
+
+
 ins = open ('tai-utc.dat' , 'r')
 array = [ ]
 
 for line in ins: 
 	array.append(line) 
-	print array
+
+with open ('tai-utc.dat') as myfile: 
+	head = [myfile.next() for x in xrange (1)] 
+	print tuple (head)
+
+	
+	
+	
