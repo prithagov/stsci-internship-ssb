741298d24a50f759553ea37353f68546e469a19a
prithagov <prithagov@yahoo.com>
2012-06-29 06:45:30 -0700

    Print '2437300.5' from the first line as a tuple, added re.search and .group() to code

--- leapseconds.py
+++ leapseconds.py
@@ -2,11 +2,11 @@ import re
 
 
 with open ('tai-utc.dat') as myfile: 
-	head = myfile.readlines()
-	head.group() 
-	print head 
-	
+	head = myfile.readline()
 	#print head
-	#head = re.sub( '\D', '',head) 
+	head = re.sub('\D','',head) 
 	#print head
-	
+	matchobj= re.search ( '2437300.5',head )
+
+if matchobj: 
+	print tuple (matchobj.group())
