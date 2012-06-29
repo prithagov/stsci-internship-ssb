b14936a7abb15e76f321392d32810d2fd2b1e381
prithagov <prithagov@yahoo.com>
2012-06-29 05:02:51 -0700

    Now prints only numbers from first line

--- leapseconds.py
+++ leapseconds.py
@@ -1,17 +1,9 @@
 import re 
 
 
-
-ins = open ('tai-utc.dat' , 'r')
-array = [ ]
-
-for line in ins: 
-	array.append(line) 
-
 with open ('tai-utc.dat') as myfile: 
-	head = [myfile.next() for x in xrange (1)] 
-	print tuple (head)
-
-	
-	
+	head = myfile.readline()
+	print head
+	head = re.sub( '\D', '',head) 
+	print head
 	
