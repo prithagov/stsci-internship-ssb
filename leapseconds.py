2974f04d37de14e35562cb2dddefb56c74bb595b
prithagov <prithagov@yahoo.com>
2012-07-03 09:54:26 -0700

    Prints out wanted numbers in each line as tuple

--- leapseconds.py
+++ leapseconds.py
@@ -11,6 +11,7 @@ def get_utc_leap_seconds():
 			
 			new=re.match('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'%((decimal_re,)*4),text).groups()
 			
+			return [new]
 		
 if __name__ == '__main__':
 	get_utc_leap_seconds()
