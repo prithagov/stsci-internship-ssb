c9389783858fdb22df0d141f5df585b2d64e1330
prithagov <prithagov@yahoo.com>
2012-07-03 09:53:51 -0700

    Prints out wanted numbers in each line as tuple

--- leapseconds.py
+++ leapseconds.py
@@ -1,16 +1,17 @@
-
-
 import re 
+import itertools as it
 
 def get_utc_leap_seconds():
-
 	with open('tai-utc.dat') as source:		
-	
+		decimal_re= '\d+\.?\d*'
+		
 		for text in source:
-			total = " " 
-			total+= text
-			matchobj = re.findall('\d+\.?\d*',total)
+			
+			
+			
+			new=re.match('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'%((decimal_re,)*4),text).groups()
+			
 		
-			print tuple (matchobj) [2:6]
-		
+if __name__ == '__main__':
+	get_utc_leap_seconds()
 	
