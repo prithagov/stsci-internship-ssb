3d8e1173a6129ddbd967a42bab821b3f5489c4b3
prithagov <prithagov@yahoo.com>
2012-07-05 10:25:09 -0700

    Prints all the tuples within one big list

--- leapseconds.py
+++ leapseconds.py
@@ -1,18 +1,23 @@
 import re 
-import itertools as it
+
 
 def get_utc_leap_seconds():
 	with open('tai-utc.dat') as source:		
 		decimal_re= '\d+\.?\d*'
 		
+		mylist = [ ]
+		
+		
 		for text in source:
 			
+			items = ( re.match('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'%((decimal_re,)*4),text).groups())
+			mylist.append(items) 
+	return mylist
 			
-			
-			new=re.match('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'%((decimal_re,)*4),text).groups()
-			
-			return [new]
 		
 if __name__ == '__main__':
-	get_utc_leap_seconds()
+	print get_utc_leap_seconds()
+		
+	
+	
 	
