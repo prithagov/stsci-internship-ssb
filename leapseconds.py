fe9113fe9a9ce1538efbd114f6ef8fba5a05cfa1
prithagov <prithagov@yahoo.com>
2012-07-06 07:24:50 -0700

    Simplified the program

--- leapseconds.py
+++ leapseconds.py
@@ -4,16 +4,11 @@ import re
 def get_utc_leap_seconds():
 	with open('tai-utc.dat') as source:		
 		decimal_re= '\d+\.?\d*'
-		
-		mylist = [ ]
-		
+
 		
 		for text in source:
 			
-			items = ( re.match('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'%((decimal_re,)*4),text).groups())
-			mylist.append(items) 
-	return mylist
-			
+			return[ ( re.match('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'%((decimal_re,)*4),text).groups()) for text in source]
 		
 if __name__ == '__main__':
 	print get_utc_leap_seconds()
