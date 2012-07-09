00a0d90f11b57c78419cf4e8d2290f3ab70fc6e7
prithagov <prithagov@yahoo.com>
2012-07-09 13:26:11 -0700

    Same output, added global variable

--- leapseconds.py
+++ leapseconds.py
@@ -2,13 +2,15 @@ import re
 
 _DECIMAL_RE = '\d+\.?\d*'
 _LEAPSECONDS_RE= re.compile('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'% ((_DECIMAL_RE,)*4))
-global source 
+source= open('tai-utc.dat').readlines()
+global leapseconds_var
+leapseconds_var= [_LEAPSECONDS_RE.match(text).groups() for text in source]	
 
 def get_utc_leap_seconds():
 	
 	with open('tai-utc.dat') as source:		
-		source.readlines
-		return [_LEAPSECONDS_RE.match(text).groups() for text in source]		
+		
+		return leapseconds_var	
 
 if __name__ == '__main__':
 	print get_utc_leap_seconds()
