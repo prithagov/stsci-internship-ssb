3728a229e121a170567c44b8c0b9fa3ba8f16fca
prithagov <prithagov@yahoo.com>
2012-07-12 05:24:32 -0700

    Few extra changes

--- leapseconds.py
+++ leapseconds.py
@@ -1,20 +1,21 @@
 import re 
 
+
 _DECIMAL_RE = '\d+\.?\d*'
+
 _LEAPSECONDS_RE= re.compile('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) X (%s)'% ((_DECIMAL_RE,)*4))
-source= open('tai-utc.dat').readlines()
-global leapseconds_var
-leapseconds_var= [_LEAPSECONDS_RE.match(text).groups() for text in source]	
+	
+
+_leap_seconds_cache = None 
 
 def get_utc_leap_seconds():
-	
+	global _leap_seconds_cache
 	with open('tai-utc.dat') as source:		
-		
-		return leapseconds_var	
+		_leap_seconds_cache = [_LEAPSECONDS_RE.match(text).groups() for text in source]
+		return _leap_seconds_cache
 
 if __name__ == '__main__':
 	print get_utc_leap_seconds()
 		
-	
 	
 	
