35a4550c8001ee2ae55923dad8312575561df8dc
prithagov <prithagov@yahoo.com>
2012-07-13 07:50:28 -0700

    the last commit was a mistake this one runs the way I want it to

--- leapseconds.py
+++ leapseconds.py
@@ -12,10 +12,10 @@ def get_utc_leap_seconds():
 	global _leap_seconds_cache
 	with open('tai-utc.dat') as source:		
 		_leap_seconds_cache = [_LEAPSECONDS_RE.match(text).groups() for text in source]
-if _leap_seconds_cache == None: 
-	return get_utc_leap_seconds()
-else:
-	return _leap_seconds_cache
+	if _leap_seconds_cache == None: 
+		return get_utc_leap_seconds()
+	else:
+		return _leap_seconds_cache
 if __name__ == '__main__':
 	print get_utc_leap_seconds()
 		
