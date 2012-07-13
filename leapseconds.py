7a220dfa697185d3a0cd9db919049877c36a662f
prithagov <prithagov@yahoo.com>
2012-07-13 12:58:00 -0700

    Possibly parts 3 and 4

--- leapseconds.py
+++ leapseconds.py
@@ -16,17 +16,18 @@ response = urllib2.urlopen('http://maia.usno.navy.mil/ser7/tai-utc.dat')
 def get_utc_leap_seconds():
 	global _leap_seconds_cache
 	
-	if _leap_seconds_cache is not None: 
-		return _leap_seconds_cache 
-	
-	elif response: 
+	try: 
 	 _leap_seconds_cache = [_LEAPSECONDS_RE.match(line).groups() for line in response]
 	 return _leap_seconds_cache
+	except IOError: 
+		print 'The program was unable to retrieve the most recent file, the information given is as accurate as possible.'
+		if _leap_seconds_cache is not None: 
+			return _leap_seconds_cache 
 	
-	else:
-		with open('tai-utc.dat') as source:		
-			_leap_seconds_cache = [_LEAPSECONDS_RE.match(text).groups() for text in source]
-			return _leap_seconds_cache	     
+		else:
+			with open('tai-utc.dat') as source:		
+				_leap_seconds_cache = [_LEAPSECONDS_RE.match(text).groups() for text in source]
+				return _leap_seconds_cache	     
 	
 if __name__ == '__main__':
 	print get_utc_leap_seconds()
