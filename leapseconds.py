c99b96e5d3867b31bf38c120d951f9ebae950375
prithagov <prithagov@yahoo.com>
2012-07-13 11:55:12 -0700

    Added elif which reads the text from the internet.

--- leapseconds.py
+++ leapseconds.py
@@ -1,5 +1,6 @@
 import re 
-
+import urllib2
+import sys
 
 _DECIMAL_RE = '\d+\.?\d*'
 
@@ -8,14 +9,24 @@ _LEAPSECONDS_RE= re.compile('.+ =JD (%s)  TAI-UTC=\s+(%s)\s+S \+ \(MJD - (%s)\) 
 
 _leap_seconds_cache = None 
 
+response = urllib2.urlopen('http://maia.usno.navy.mil/ser7/tai-utc.dat') 
+
+
+
 def get_utc_leap_seconds():
 	global _leap_seconds_cache
-	if _leap_seconds_cache !=None: 
+	
+	if _leap_seconds_cache is not None: 
 		return _leap_seconds_cache 
-	else: 
+	
+	elif response: 
+	 _leap_seconds_cache = [_LEAPSECONDS_RE.match(line).groups() for line in response]
+	 return _leap_seconds_cache
+	
+	else:
 		with open('tai-utc.dat') as source:		
-			 _leap_seconds_cache = [_LEAPSECONDS_RE.match(text).groups() for text in source]
-			 return _leap_seconds_cache	     
+			_leap_seconds_cache = [_LEAPSECONDS_RE.match(text).groups() for text in source]
+			return _leap_seconds_cache	     
 	
 if __name__ == '__main__':
 	print get_utc_leap_seconds()
